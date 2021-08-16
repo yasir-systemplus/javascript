a372f7b8d53b60b3665b6e4f60d4eab03e7b608d
= <=>
2021-08-16 13:10:00 +0500

    Done with template literals

---
+++ app.js
@@ -0,0 +1,9 @@
+function greetings(name, item1, item2) {
+  console.log(`
+  Hello: ${name}, 
+  Item 1: ${item1}, 
+  Item 2: ${item2}, 
+  Grand Total: ${item2 + item1}`);
+}
+
+greetings("Yasir", 34, 56);
