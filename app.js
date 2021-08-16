0d4c33cd09f9ae996af8c853317dff53ac41b0b5
= <=>
2021-08-16 16:58:12 +0500

    Done with Array Methods

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const group1 = ["Bilal", "Farhan", "Aqib"];
 const group2 = ["Jameel", "Adill", "Nouman"];
 
-const all = [...group1, ...group2];
-console.log(all);
+const [first, second, ...all] = group2;
+console.log(first, second, all);
+
+console.log(group1.includes("Bilal"));
