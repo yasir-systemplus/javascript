8c7bc8f9034dc993a3f5d3c18618436d02cac4a0
= <=>
2021-08-17 16:26:02 +0500

    Chapter 4: Error message

--- functional-programing/src/app.js
+++ functional-programing/src/app.js
@@ -1,34 +1,48 @@
-import words from "an-array-of-english-words";
+const currentInputValues = {
+  firstName: "Shaun", // Must be at least 2 characters
+  lastName: "", // Must be at least than 2 characters
+  zipCode: "12345", // Must be exactly 5 characters
+  state: "", // Must be exactly 2 characters
+};
 
-const countOccurrences = (arr) =>
-  arr.reduce(
-    (acc, str) => ({
-      ...acc,
-      [str]: acc[str] ? acc[str] + 1 : 1,
-    }),
-    {}
-  );
+const inputCriteria = {
+  firstName: [
+    (value) =>
+      value.length >= 2 ? "" : "First name must be at least 2 characters",
+  ],
+  lastName: [
+    (value) =>
+      value.length >= 2 ? "" : "Last name must be at least 2 characters",
+  ],
+  zipCode: [
+    (value) =>
+      value.length === 5 ? "" : "Zip must be exactly 5 characters long",
+  ],
+  state: [
+    (value) =>
+      value.length === 2 ? "" : "State must be exactly 2 characters long",
+  ],
+};
 
-const hasSameLetterCount = (word1, word2) => {
-  const word1Count = countOccurrences(word1.split(""));
-  const word2Count = countOccurrences(word2.split(""));
-
-  return (
-    Object.keys(word1Count).length === Object.keys(word2Count).length &&
-    Object.keys(word1Count).every(
-      (letter) => word1Count[letter] === word2Count[letter]
+const getErrorMessages = (inputs, criteria) => {
+  return Object.keys(inputs)
+    .reduce(
+      (acc, fieldName) => [
+        ...acc,
+        ...criteria[fieldName].map((test) => test(inputs[fieldName])),
+      ],
+      []
     )
-  );
+    .filter((message) => message);
 };
 
-const findAnagrams = (word, allWords) => {
-  return allWords
-    .filter((entry) => hasSameLetterCount(word, entry))
-    .filter((anagram) => anagram !== word);
-};
-
-console.log(findAnagrams("cinema", words));
+console.log(getErrorMessages(currentInputValues, inputCriteria));
 
 /*
-    Expected output: ['iceman', 'anemic']
+  Expected Output: [
+      'First name must be at least 2 characters',
+      'Last name must be at least 2 characters',
+      'Zip code must be exactly 5 characters',
+      'State must be exactly 2 characters',
+  ]
 */
