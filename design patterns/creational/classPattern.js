a5506a6a7f985cf9c786821c068eee7717042c2a
= <=>
2021-08-20 11:09:36 +0500

    Class Pattern

---
+++ design patterns/creational/classPattern.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(name, age, ssn) {
+    this._name = name;
+    this._age = age;
+    this._ssn = ssn;
+  }
+}
+
+const bilal = new Person("Bilal Ahmad", 23, "1231@31");
+
+console.log(bilal);
