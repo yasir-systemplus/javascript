eb77baa1bfdccbed8ae1bbe5961310376f93dde3
= <=>
2021-08-23 11:05:06 +0500

    Checking the order of promise execution

--- javascript-async/app.js
+++ javascript-async/app.js
@@ -1,1 +1,18 @@
-console.log("object");
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Hello from P1");
+  }, 5000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Hello from P2");
+  }, 2000);
+});
+
+const info = (info) => {
+  console.log(info);
+};
+
+p1.then(info);
+p2.then(info);
